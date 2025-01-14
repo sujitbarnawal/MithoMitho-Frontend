79cea7849d59853a7b2dc4438512bca3f8ca0a16
sujitbarnawal <sujitbarnawal4545@gmail.com>
2025-01-14 14:52:16 +0545

    navbar for smaller screensizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,13 +54,13 @@ const Navbar = ({setShowLogin}) => {
           </NavLink>
         </li>
       </ul>
-      <div className="flex items-center gap-10 max-1050:gap-[30px] max-900:gap-5 ">
+      <div className="flex items-center gap-10 max-1050:gap-[30px] max-900:gap-5 max-480:pl-5 ">
         <img className="max-1050:w-[22px] max-900:w-5 " src={assets.search_icon} alt="" />
         <div className="relative">
           <img className="cursor-pointer" onClick={()=>navigate('/cart')} src={assets.basket_icon} alt="" />
           <div className={getTotalCartAmount()===0?"":"absolute min-w-2.5 min-h-2.5 bg-[tomato] rounded-md top-[-8px] right-[-5px]"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)} className="bg-transparent text-[16px] text-[#49557e] border-[1px] border-solid border-[tomato] py-2.5 px-7 rounded-full cursor-pointer hover:bg-[#fff4f2] transition max-1050:py-2 max-1050:px-[25px] max-900:py-[7px] max-900:px-5 max-900:text-[15px] ">
+        <button onClick={()=>setShowLogin(true)} className="bg-transparent text-[16px] text-[#49557e] border-[1px] border-solid border-[tomato] py-2.5 px-7 rounded-full cursor-pointer hover:bg-[#fff4f2] transition max-1050:py-2 max-1050:px-[25px] max-900:py-[7px] max-900:px-5 max-900:text-[15px] max-480:text-[12px]">
           Sign in
         </button>
       </div>
